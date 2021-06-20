7152813da82d6e3e6f027d462b3714531365e883
SanariSan <SanariSan@users.noreply.github.com>
2021-06-20 22:13:03 +0300

    changes in routes

---
+++ src/controllers/get-info/groups-info.controller.ts
@@ -0,0 +1,37 @@
+import { Response, NextFunction } from "express";
+import { SuccessResponse } from "../../core";
+import { EGROUP_KEYS, EGROUP_PATH_KEYS, EGROUP_RELATIONS } from "../../database/connection";
+import { ProtectedRequest } from "../../types";
+
+// req.body.send === {groupInfo: {id, name}, storageSize: {sizeUsed, sizeMax}}
+// req.body === id: groupId
+export const GroupInfo = async (req: ProtectedRequest, res: Response, next: NextFunction) => {
+	let result: Array<{ ownerId: number; groupId: number }> = [];
+
+	await req.groupRepository.findById(req.body.id, [EGROUP_RELATIONS.GROUP_PATH]);
+	const groupRecord = req.groupRepository.getRecord();
+	if (!groupRecord) throw new Error();
+
+	await req.groupPathRepository.findById(groupRecord.groupPath.id);
+	const groupPathRecord = req.groupPathRepository.getRecord();
+	if (!groupPathRecord) throw new Error();
+
+	const userRecord = req.userRepository.getRecord();
+	if (!userRecord) throw new Error();
+
+	if (userRecord.groupOwnage)
+		result = [
+			{
+				ownerId: userRecord.id,
+				groupId: userRecord.groupOwnage.id,
+			},
+		];
+
+	return new SuccessResponse("Groups found", {
+		groupInfo: req.groupRepository.getRecord([EGROUP_KEYS.ID, EGROUP_KEYS.NAME]),
+		storageSize: req.groupPathRepository.getRecord([
+			EGROUP_PATH_KEYS.SIZE_USED,
+			EGROUP_PATH_KEYS.SIZE_MAX,
+		]),
+	}).send(res);
+};
--- src/routers/api/v1/v1.router.ts
+++ src/routers/api/v1/v1.router.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import { AccessRouter } from "./access";
 import { FilesRouter } from "./fs";
 import { GroupRouter } from "./group";
-import { ProfileRouter } from "./profile";
+import { GetInfoRouter } from "./get-info";
 
 const V1 = Router();
 
 V1.use("/access", AccessRouter);
-V1.use("/profile", ProfileRouter);
+V1.use("/info", GetInfoRouter);
 V1.use("/group", GroupRouter);
 V1.use("/fs", FilesRouter);
 
