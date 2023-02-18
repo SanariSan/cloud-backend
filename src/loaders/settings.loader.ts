2ea27012f335790e34af04268cfeefe9d9094e23
SanariSan <SanariSan@users.noreply.github.com>
2023-02-18 17:44:33 +0400

    fix: loosen cors, +helmet

--- src/loaders/settings.loader.ts
+++ src/loaders/settings.loader.ts
@@ -1,20 +1,23 @@
 import cors from "cors";
 import express from "express";
 import path from "path";
+import helmet from "helmet";
 
 function settings(app) {
 	app.use(
 		cors({
-			// origin: true,
-			origin:
-				process.env.NODE_ENV === "production" ? `https://${process.env.CORS_URL}` : true,
+			origin: true,
+			// origin:
+			// 	process.env.NODE_ENV === "production" ? `https://${process.env.CORS_URL}` : true,
 			credentials: true,
 			optionsSuccessStatus: 204,
 		}),
 	);
 	app.enable("trust proxy");
+	app.use(helmet());
 	app.use(express.json({ limit: "100mb" }));
 	app.use(express.urlencoded({ limit: "100mb", extended: true }));
+	app.set("x-powered-by", false);
 
 	if (process.env.NODE_ENV === "production") {
 		app.use(express.static(path.resolve("./cloud-front-build")));
